9f77f638c757456cd019b4632384ad44ae8de836
vijai-ks <vijai.ksv@gmail.com>
2023-10-02 16:13:51 +0530

    refactor: input_page: Update the customized widget
    Create a separate class for customized widget to reuse
    
    Signed-off-by: vijai-ks <vijai.ksv@gmail.com>

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -30,24 +30,9 @@ class _InputPageState extends State<InputPage> {
                   Expanded(
                     child: ReusableCard(
                       colour: primaryCardColor,
-                      childCard: const Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            FontAwesomeIcons.mars,
-                            size: 100.0,
-                          ),
-                          SizedBox(
-                            height: 15.0,
-                          ),
-                          Text(
-                            'MALE',
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              color: Color(0xFF8D8E98),
-                            ),
-                          )
-                        ],
+                      childCard: GenderCards(
+                        genderIcon: FontAwesomeIcons.mars,
+                        gender: 'MALE',
                       ),
                     ),
                   ),
@@ -96,6 +81,36 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class GenderCards extends StatelessWidget {
+  String gender;
+  IconData genderIcon;
+
+  GenderCards({required this.gender, required this.genderIcon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          genderIcon,
+          size: 100.0,
+        ),
+        const SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          gender,
+          style: const TextStyle(
+            fontSize: 20.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
   Color colour;
   Widget childCard;
@@ -105,12 +120,12 @@ class ReusableCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: childCard,
       decoration: BoxDecoration(
         color: colour,
         borderRadius: BorderRadius.circular(10.0),
       ),
       margin: const EdgeInsets.all(15.0),
+      child: childCard,
     );
   }
 }
