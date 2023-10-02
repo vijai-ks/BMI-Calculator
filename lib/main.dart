7d4a6c63b344e735f2913666c4c2013bdf1e4a2d
vijai-ks <vijai.ksv@gmail.com>
2023-10-02 08:29:31 +0530

    style: bmi_calculator: Add themes to the screen
    Signed-off-by: vijai-ks <vijai.ksv@gmail.com>

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xff0a0e21),
+        ),
+      ),
+      home: const InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('BMI Calculator'),
+        ),
+      ),
+      body: const SafeArea(
+        child: Center(
+          child: Text('Body here'),
+        ),
+      ),
+    );
+  }
+}
